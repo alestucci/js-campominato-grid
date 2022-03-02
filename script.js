e4e68c667b930d3d282e1bf0c85a2fdd9eda42ef
alestucci <alessandrotucci@gmail.com>
2022-03-02 15:30:57 +0100

    Grid completed

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 const difficulty = document.getElementById("difficulty").value;
 console.log(difficulty);
 
-let column;
+const field = document.querySelector(".field");
+console.log(field);
+let squares;
 
 switch (difficulty) {
-	case 'easy':
-		column = 7;
+	case "easy":
+		squares = 49;
+		field.classList.add("field-49");
 		break;
-	case 'medium':
-		column = 9;
+	case "medium":
+		squares = 81;
+		field.classList.add("field-81");
 		break;
-	case 'hard':
-		column = 10;
+	case "hard":
+		squares = 100;
+		field.classList.add("field-100");
 		break;
 }
 
-console.log(column);
+console.log(squares);
 
-
+for (let index = 1; index <= squares; index++) {
+	let box = document.createElement("div");
+	box.classList.add("square");
+	box.innerHTML = index;
+	field.append(box);
+}
