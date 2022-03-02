2a9c8323ab221ab5a0f69fa884ba8451f5080248
alestucci <alessandrotucci@gmail.com>
2022-03-02 19:31:24 +0100

    Job finished!

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const difficulty = document.getElementById("difficulty").value;
-console.log(difficulty);
+let playBtn = document.getElementById("play");
+//console.log(playBtn);
+playBtn.addEventListener("click", function () {
+	const difficulty = document.getElementById("difficulty").value;
+	//console.log(difficulty);
+	const field = document.querySelector(".field");
+	//console.log(field);
+	field.innerHTML = "";
+	field.classList = "field";
+	let squares = 0;
 
-const field = document.querySelector(".field");
-console.log(field);
-let squares;
+	switch (difficulty) {
+		case "easy":
+			squares = 49;
+			field.classList.add("field-49");
+			break;
+		case "medium":
+			squares = 81;
+			field.classList.add("field-81");
+			break;
+		case "hard":
+			squares = 100;
+			field.classList.add("field-100");
+			break;
+	}
+	//console.log(squares);
+	for (let index = 1; index <= squares; index++) {
+		let box = document.createElement("div");
+		box.classList.add("square");
+		box.id = "n" + index;
+		box.innerHTML = index;
+		box.addEventListener("click", function () {
+			squareSelection(box);
+		});
+		field.append(box);
+	}
+});
 
-switch (difficulty) {
-	case "easy":
-		squares = 49;
-		field.classList.add("field-49");
-		break;
-	case "medium":
-		squares = 81;
-		field.classList.add("field-81");
-		break;
-	case "hard":
-		squares = 100;
-		field.classList.add("field-100");
-		break;
-}
-
-console.log(squares);
-
-for (let index = 1; index <= squares; index++) {
-	let box = document.createElement("div");
-	box.classList.add("square");
-	box.innerHTML = index;
-	field.append(box);
+function squareSelection(target) {
+	target.classList.toggle("selected");
 }
